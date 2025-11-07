225eb8105b2f18fdafbb3551e914a4813ba7ecca
bangsyir <bangsyir@gmail.com>
2025-11-07 11:36:33 +0800

    change prettier to prettierd

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,12 +5,12 @@ return {
     opts.formatters_by_ft = vim.tbl_extend("force", opts.formatters_by_ft or {}, {
       lua = { "stylua" },
       go = { "goimports", "gofmt" },
-      javascript = { "prettier" },
-      css = { "prettier" },
-      html = { "prettier" },
-      typescript = { "prettier" },
-      typescriptreact = { "prettier" },
-      json = { "prettier" },
+      javascript = { "prettierd" },
+      css = { "prettierd" },
+      html = { "prettierd" },
+      typescript = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      json = { "prettierd" },
     })
   end,
 }
