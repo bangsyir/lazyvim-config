28d8cf3d4661ae409722fe6163789feb2936cbcd
bangsyir <bangsyir@gmail.com>
2025-11-07 11:36:49 +0800

    update package

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.clipboard = ""
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+-- Optional: to clearly mark the visual break
+vim.opt.showbreak = "↪"
